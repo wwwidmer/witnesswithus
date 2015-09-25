2db7b6cdb91b9623a97a255ceb96463afb402714
william widmer <williamwidmerr@gmail.com>
2015-09-24 21:17:17 -0400

    grab by url better

--- static/witness.js
+++ static/witness.js
@@ -6,8 +6,12 @@ $("button").on('click', function(){
     var split = url.replace(/.*?:\/\//g, "");
     var host = split.split('/')[0];
     host = $.trim(host);
-    var re = /\d+/;
-    var vid = re.exec(split);
+    if(host == 'meerkatapp.co'){
+	var vid = split.split('/')[2];
+    } else {
+	var re = /\d+/;
+	var vid = re.exec(split);
+    }
     var newurl = window.location.href+"counter?host="+host+"&vid="+vid;
     $("#newurl").html("<a href='"+newurl+"'>"+newurl+"</a>");
 });
