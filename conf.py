d697dce7caeb0b5a2497c8b773b48d7df440e743
billy <williamwidmerr@gmail.com>
2016-06-21 19:50:22 -0400

    added notes

--- conf.py
+++ conf.py
@@ -4,7 +4,8 @@ ACCEPTED_HOSTS = secrets.HOSTS
 
 BAMBUSER_API_KEY = secrets.API_KEYS['bambuser']['api_key']
 BAMBUSER_USERNAME = secrets.API_KEYS['bambuser']['user']
+'''
 MEERKAT_API_KEY = secrets.API_KEYS['meerkat']
 PERISCOPE_API_KEY = secrets.API_KEYS['periscope']
-# for a tool we're not using currently but good to have
 LIVESTREAM_API_KEY = secrets.API_KEYS['livestream']
+'''
